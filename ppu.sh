2ebdabb4f4f5d2699e4f856e85fa83e6e9a29231
Faiz Ali <ali.faiz019@gmail.com>
2015-06-18 14:41:22 -0400

    ppu.sh

--- ppu.sh
+++ ppu.sh
@@ -5,112 +5,94 @@ set -e
 # By: Faiz Ali
 # ali.faiz019@gmail.com
 
-# (done) Check if jails dataset exists; quit if doesn't exist
-# (done) Check if username exits
-# (done) Two different log files; list of all jails; list of all changes to the jails [year/month/day:hour:min:sec]
-# (done) Delete operation
-# (done) List jails
-# (done) 2 spaces for indentation
-# (done-ish) make everything a separate function
-# (done) print errors to error stream &2 echo "# - error" exit #
-# (done-ish)get IP from log instead of ping
-# (done) create configuration file ppu.conf
-# (done-ish) test create and delete jail in same statement; generate random student
-# Change password
-
 # Parameters from ppu.conf
 poolname=`cat ppu.conf | grep poolname | sed "s|poolname=||g"`
 ipaddress=`cat ppu.conf | grep ipaddress | sed "s|ipaddress=||g"`
 iptest=`cat ppu.conf | grep ipteststart | sed "s|ipteststart=||g"`
-list=`cat ppu.conf | grep jaillist | sed "s|jaillist=||g"`jaillist.txt
-log=`cat ppu.conf | grep jaillog | sed "s|jaillog=||g"`jaillog.txt
+location=`cat ppu.conf | grep location | sed "s|location=||g"`
 
 # Script parameters
 action=$1
 username=$2
+list=$location/jaillist.txt
+log=$location/jaillog.txt
 
 createjail() {
   #Check if jails dataset doesn't exist; end if it doesn't
-  if [ ! -d "/usr/jails" ] 
+  if [ ! -d "$location" ] 
 		then 
       echo "1 - The jails dataset does not exist" 1>&2
       exit 1
 	fi
 	
 	#Check if username already exists; end if it does
-	if [ -d "/usr/jails/$username" ] 
+	if [ -d "$location/$username" ] 
 		then
       echo "2 - A jail for $username already exists" 1>&2
       exit 2
 	fi
 	
 	#Create dataset for each username/jail; mount to jails location
-	#zfs create $poolname/jails/$username
-	#zfs set mountpoint=/usr/jails/$username $poolname/jails/$username
-	#ipfind=1
-
-  echo "hi"
-
+	zfs create $poolname/jails/$username
+	zfs set mountpoint=$location/$username $poolname/jails/$username
+	ipfind=1
+  
 	#Check each IP for ping response, if no response IP is not being used
 	while [ $ipfind -eq 1 ]
-	do
-    check=`cat $list | grep $ipaddress$iptest`
-    echo $check
-    #if [ $check = ];
-		#if [[ ! -z "$check" ]]
+	do 
+    check=`grep $ipaddress$iptest $list || true`    
+		if [ -n "$check" ]
     then	
 			iptest=`expr $iptest + 1` 
 		else
       ipfind=0	
 		fi
 	done
-	
-  test=$ipaddress$iptest
-  echo $test
   
 	#Create a jail with username/password $username and ask to change password on logging in
-	#qjail create -c -4 $ipaddress$iptest $username
+	qjail create -c -4 $ipaddress$iptest $username
 	
 	#Log list of all created and active jails 
-	#echo $username $ipaddress$iptest host$iptest.cmps.pointpark.edu >> $list
+	echo $username $ipaddress$iptest host$iptest.cmps.pointpark.edu >> $list
 	
 	#Log action CREATE taken on a jail
-	#echo `date +"[%y/%m/%d:%I:%M:%S]"` CREATE $username $ipaddress$iptest `who | awk '{print $1}'` >> $log
+	echo `date +"[%y/%m/%d:%I:%M:%S]"` CREATE $username $ipaddress$iptest `who | awk '{print $1}'` >> $log
 
 	#Start jail
-	#qjail start $username
+	qjail start $username
 }
 
-# deletejail() {
-  # #Check if username exists; end if it doesn't
-  # if [ ! -d "/usr/jails/$username" ] 
-    # then
-      # echo "3 - $username does not exist" 1>&2 
-      # exit 3
-  # fi
+ deletejail() {
+  #Check if username exists; end if it doesn't
+  if [ ! -d "$location/$username" ] 
+    then
+      echo "3 - $username does not exist" 1>&2 
+      exit 3
+  fi
   
-  # #Get jail IP
-  # ip=`cat $list | grep $username | awk '{print $2}'`
+  #Get jail IP
+  ip=`cat $list | grep $username | awk '{print $2}'`
 
-  # #Stop jail, remove it, unmount dataset, remove it, remove remaining directory
-  # qjail stop $username
-  # qjail delete $username
-  # zfs unmount -f /usr/jails/$username #doesn't work without -f?
-  # zfs destroy $poolname/jails/$username
-  # rmdir /usr/jails/$username
+  #Stop jail, remove it, unmount dataset, remove it, remove remaining directory
+  qjail stop $username
+  qjail delete $username
+  zfs unmount -f $location/$username #doesn't work without -f?
+  zfs destroy $poolname/jails/$username
+  rmdir $location/$username
   
-  # #Update list of all jails
-  # sed -i '' '/'$username'/ d' $list
+  #Update list of all jails
+  sed -i '' '/'$username'/ d' $list
   
-  # #Log action DELETE action
-   # echo `date +"[%y/%m/%d:%I:%M:%S]"` DELETE $username $ip `who -m | awk '{print $1}'` >> $log
- # }
+  #Log action DELETE action
+  echo `date +"[%y/%m/%d:%I:%M:%S]"` DELETE $username $ip `who -m | awk '{print $1}'` >> $log
+}
 
-#jailtest() {
-  #username=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1) 
-  #createjail
-  #deletejail
-# }
+jailtest() {
+  username=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1) 
+  echo $username
+  createjail
+  deletejail
+}
 
 #Create a jail "ppu.sh createjail username"
 if [ $action = "createjail" ]
@@ -133,12 +115,7 @@ elif [ $action = "log" ]
 		cat /usr/jails/jaillog.txt
 
 #Create a jail for random student; delete the jail "ppu.sh jailtest"
-elif [ $action = "test" ]
+elif [ $action = "jailtest" ]
 	then
-    #jailtest
+    jailtest
 fi
-
-
-
-
-
