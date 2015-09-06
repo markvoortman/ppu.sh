8b85325ea290d441e08e1bbcc9ed791c7016bc5f
Mark Voortman <mark@voortman.name>
2015-09-06 09:37:18 -0400

    Use full path for poudriere.

--- ppu.sh
+++ ppu.sh
@@ -220,9 +220,9 @@ password() {
 
 buildpkg() {
   # build package list
-  poudriere jail -u -j freebsd_10-1x64
-  poudriere ports -u -p HEAD
-  poudriere bulk -j freebsd_10-1x64 -p HEAD -f /usr/local/etc/poudriere.d/port-list
+  /usr/local/bin/poudriere jail -u -j freebsd_10-1x64
+  /usr/local/bin/poudriere ports -u -p HEAD
+  /usr/local/bin/poudriere bulk -j freebsd_10-1x64 -p HEAD -f /usr/local/etc/poudriere.d/port-list
 }
 
 editpkg() {
