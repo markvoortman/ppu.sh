3d67f815f280bec768d31849b47cd9604cae4a9e
Mark Voortman <mvoortman@pointpark.edu>
2021-06-27 10:21:28 -0400

    Update to 12.2.

--- ppu.sh
+++ ppu.sh
@@ -301,9 +301,9 @@ password() {
 
 buildpkg() {
   # build package list
-  /usr/local/bin/poudriere jail -u -j 114amd64
+  /usr/local/bin/poudriere jail -u -j 122amd64
   /usr/local/bin/poudriere ports -u -p quarterly
-  /usr/local/bin/poudriere bulk -j 114amd64 -p quarterly -f /usr/local/etc/poudriere.d/port-list
+  /usr/local/bin/poudriere bulk -j 122amd64 -p quarterly -f /usr/local/etc/poudriere.d/port-list
 }
 
 editpkg() {
